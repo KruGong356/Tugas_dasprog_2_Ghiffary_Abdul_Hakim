709b04ee466a6a5b7b5da19cb9bb7a50d7caa1f6
KruGong <139752381+KruGong356@users.noreply.github.com>
2024-09-10 22:55:23 +0700

    Add files via upload

---
+++ HR_LSM2.py
@@ -0,0 +1,12 @@
+x, z = map(int, input().split())
+
+if(z == 1):
+    if(x % 3 == 0):
+        print("lili")
+    else:
+        print("lala")
+else:
+    if(x % 3 == 0):
+        print("lala")
+    else:
+        print("lili")
